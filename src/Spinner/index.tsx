56d52c57e5975bf5d53b24d75c4261c9037eb71c
Jonathan Rupp <jrupp@rightpoint.com>
2019-04-02 21:02:09 -0500

    Add storybook and a spinner to demonstrate how fast react renders

---
+++ src/Spinner/index.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+
+export interface SpinnerProps {
+    startNumber: number;
+    endNumber: number;
+    increment: number;
+    speed: number;
+    precision?: number;
+}
+
+interface SpinnerState {
+    currentNumber: number;
+}
+
+export default class Spinner extends React.Component<SpinnerProps, SpinnerState> {
+    private timeout?: number;
+    constructor(props: SpinnerProps) {
+        super(props);
+        this.state = {
+            currentNumber: props.startNumber,
+        };
+    }
+
+    public componentDidMount() {
+        this.start();
+    }
+
+    public componentDidUpdate(prevProps: SpinnerProps) {
+        if (this.props.startNumber !== prevProps.startNumber || this.props.endNumber !== prevProps.endNumber || this.props.increment !== prevProps.increment || this.props.speed !== prevProps.speed) {
+            this.start();            
+        }
+    }
+
+    public render() {
+        const number = this.props.precision === undefined ? this.state.currentNumber : this.state.currentNumber.toFixed(this.props.precision);
+        return (
+            <div className="spinner">
+                <div className="number">{number}</div>
+            </div>
+        );
+    }
+
+    private start = () => {
+        if (this.timeout) {
+            window.clearTimeout(this.timeout);
+        }
+        this.setState({
+            currentNumber: this.props.startNumber,
+        }, this.queueIncrement);
+    }
+
+    private doIncrement = () => {
+        if (this.props.increment > 0 && this.state.currentNumber >= this.props.endNumber) {
+            return;
+        }
+        if (this.props.increment < 0 && this.state.currentNumber <= this.props.endNumber) {
+            return;
+        }
+        if (this.props.increment === 0) {
+            return;
+        }
+
+        this.setState({
+            currentNumber: this.props.increment > 0 
+                ? Math.min(this.state.currentNumber + this.props.increment, this.props.endNumber)
+                : Math.max(this.state.currentNumber + this.props.increment, this.props.endNumber)
+        }, this.queueIncrement)
+    }
+
+    private queueIncrement = () => {
+        this.timeout = window.setTimeout(this.doIncrement, this.props.speed);
+    }
+}
---
+++ stories/index.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
+
+import { Button, Welcome } from '@storybook/react/demo';
+import Spinner from '../src/Spinner';
+
+storiesOf('Spinner', module)
+  .addDecorator(withKnobs)
+  .add('Spinner', () => {
+    const startNumber = number('Start value', 0);
+    const endNumber = number('End value', 90.3);
+    const increment = number('Increment', 1.5);
+    const precision = number('Precision', 1);
+    const speed = number('Speed', 0);
+    const key = text('Key', 'x');
+    const props = {
+      startNumber,
+      endNumber,
+      increment,
+      precision,
+      speed,
+      key,
+    };
+
+    return <Spinner {...props}/>;
+  });
