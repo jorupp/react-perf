f0103c0ded6581a5ab08821baa98a2c212cc2804
Jonathan Rupp <jrupp@rightpoint.com>
2019-04-02 21:19:32 -0500

    Add multiple spinners

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -8,7 +8,7 @@ import LoggingWrapper from '../src/LoggingWrapper';
 
 storiesOf('Spinner', module)
   .addDecorator(withKnobs)
-  .add('Spinner', () => {
+  .add('Single Spinner', () => {
     const startNumber = number('Start value', 0);
     const endNumber = number('End value', 90.3);
     const increment = number('Increment', 1.5);
@@ -25,4 +25,34 @@ storiesOf('Spinner', module)
     };
 
     return <LoggingWrapper><Spinner {...props}/></LoggingWrapper>;
-  });
+  })
+  .add('Multiple Spinners', () => {
+    const startNumber = number('Start value', 0);
+    const increment = number('Increment', 1.5);
+    const precision = number('Precision', 1);
+    const speed = number('Speed', 0);
+    const key = text('Key', 'x');
+
+    const endNumbers = [
+      number('End value 1', 90.3),
+      number('End value 2', 80.2),
+      number('End value 3', 50.9),
+      number('End value 4', 4.8),
+      number('End value 5', 23.7),
+      number('End value 6', 40.8),
+    ]
+
+    const props = {
+      startNumber,
+      increment,
+      precision,
+      speed,
+    };
+
+    return <LoggingWrapper key={key}>
+      {
+        endNumbers.map((endNumber, ix) => <Spinner key={ix} {...props} endNumber={endNumber}/>)
+      }
+    </LoggingWrapper>;
+  })
+;
